ddb7ad0946871691ba9c2973205d0d120fc56356
Finn <finley.champion@gmail.com>
2024-10-02 14:51:11 +0100

    progress in adding a slider

--- script.js
+++ script.js
@@ -5,14 +5,39 @@
   const handleSelect = (e) => {
 
         e.preventDefault();
-        const selectedOption = document.querySelector('#pageviews').value;
+        const selectedOption = document.querySelector('#pageviewsSlider').value;
         console.log('You selected:', selectedOption);
-        document.querySelector('#displayPrice').textContent = selectedOption
+        document.querySelector('#displayNumb').textContent = selectedOption
+        
+        
+        if (selectedOption == 1) {
+          document.querySelector('#displayPrice').textContent = '$8.00'
+        }
+        if (selectedOption == 2) {
+          document.querySelector('#displayPrice').textContent = '$12.00'
+        }
+        if (selectedOption == 3) {
+          document.querySelector('#displayPrice').textContent = '$16.00'
+        }
+        if (selectedOption == 4) {
+          document.querySelector('#displayPrice').textContent = '$24.00'
+        }
+        if (selectedOption == 5) {
+          document.querySelector('#displayPrice').textContent = '$36.00';
+         
+        }
         
     
 }
 
-document.querySelector('#myForm').addEventListener('submit', handleSelect);
+document.querySelector('#myForm').addEventListener('input', handleSelect);
+
+
+
+
+
+
+
 
 
 
