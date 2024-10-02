37940d263334f68d57fa3ecdff6c43876a801ad2
Finn <finley.champion@gmail.com>
2024-10-02 16:50:46 +0100

    jsut need to add default value

--- script.js
+++ script.js
@@ -1,58 +1,8 @@
 'use strict';
 
-  
-
-  const handleSelect = (e) => {
-
-        const selectedOption = document.querySelector('#page-views-slider').value;
-        const boxChecked = document.querySelector('#yearly-discount');
-        console.log('You selected:', selectedOption);
-        console.log('You selected:', boxChecked);
-        let price, users;
-
-        if (selectedOption == 1) {
-          price = 8.00
-          users = '10k'
-          
-        }
-        if (selectedOption == 2) {
-          price = 12.00
-          users = '50k'
-        
-        }
-        if (selectedOption == 3) {
-          price = 16.00
-          users = '100k'
-          
-        }
-        if (selectedOption == 4) {
-          price = 24.00
-          users = '500k'
-        
-        }
-        if (selectedOption == 5) {
-          price = 36.00
-          users = '1M'
-          
-        }
-
-        if (boxChecked.checked) {
-          price *= 12
-          price *= 0.75
-          
-        }
-
-        document.querySelector('#display-price').textContent = '$' + price;
-        document.querySelector('#display-users').textContent = users;  
-        console.log('You selected:', price);
-}
-
-document.querySelector('#my-form').addEventListener('input', handleSelect);
-
-
 const viewings = [
   {
-    view: '10K pageviews',
+    view: '10K PAGEVIEWS',
     price: 8
     },
     {
@@ -73,6 +23,35 @@ const viewings = [
     }
   
 ]
+
+  const handleSelect = (e) => {
+
+        const selectedOption = document.querySelector('#page-views-slider').value;
+        const boxChecked = document.querySelector('#yearly-discount');
+        console.log('You selected:', selectedOption);
+        console.log('You selected:', boxChecked);
+        let price = viewings[selectedOption]['price']
+        let users = viewings[selectedOption]['view']
+
+        console.log(price)
+        console.log(viewings)
+
+
+        if (boxChecked.checked) {
+          price *= 12
+          price *= 0.75
+          
+        }
+
+        document.querySelector('#display-price').textContent = '$' + price;
+        document.querySelector('#display-users').textContent = users;  
+        console.log('You selected:', price);
+}
+
+document.querySelector('#my-form').addEventListener('input', handleSelect);
+
+
+
 
 
 
